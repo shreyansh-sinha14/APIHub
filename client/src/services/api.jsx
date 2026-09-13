00588c8e71988a248e7e088326fcf08ce5138c9c
SHREYANSH SINHA <sinha.14shreyansh@gmail.com>
2026-09-14 01:28:58 +0530

    Connect frontend to deployed backend

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+const API = axios.create({
+    baseURL: "https://apihubbackend.onrender.com/api",
 });
 
-export default api;
+export default API;
